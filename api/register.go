3b3420d2f27ec140b186384b69c9d8c61186242b
Timo <artur.fiedler54@gmail.com>
2019-12-18 20:59:31 +0100

    append new webhook to array

--- api/register.go
+++ api/register.go
@@ -1,12 +1,24 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 
 	"github.com/y21/wrnotifier-go/structures"
 )
 
 // Register is used to register a new webhook
 func Register(w http.ResponseWriter, r *http.Request, webhooks *[]structures.Webhook) {
+	params := mux.Vars(r)
+	*webhooks = append(*webhooks, structures.Webhook{
+		EngineClass150: false,
+		ID:             params["id"],
+		Server:         "",
+		Token:          params["token"],
+	})
 
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "{\"status\":%d}", 200)
 }
