3cb5f4d321a650359b0336aac61ab4f0a9ec4ffa
Timo <artur.fiedler54@gmail.com>
2019-12-18 22:55:45 +0100

    sync with local filE2

--- worker/worker.go
+++ worker/worker.go
@@ -1,7 +1,11 @@
 package worker
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"time"
+	"fmt"
+	"os"
 
 	"github.com/y21/wrnotifier-go/structures"
 )
@@ -10,10 +14,28 @@ func work(webhooks *[]structures.Webhook) {
 
 }
 
+func updateLocalCopy(webhooks *[]structures.Webhook, sync *bool) {
+	if !*sync {
+		fmt.Println("Updating file...")
+		bytes, err := json.MarshalIndent(*webhooks, "", " ")
+		if err != nil {
+			fmt.Printf("An error occurred while trying to decode JSON: %s", err)
+			os.Exit(1)
+		}
+		err = ioutil.WriteFile("webhooks.json", bytes, 0644)
+		if err != nil {
+			fmt.Printf("An error occurred while trying to write to file: %s", err)
+			os.Exit(1)
+		}
+		*sync = true
+	}
+}
+
 // Loop is used to concurrently fetch data
-func Loop(webhooks *[]structures.Webhook) {
+func Loop(webhooks *[]structures.Webhook, sync *bool) {
 	for {
 		time.Sleep(3 * time.Second)
 		go work(webhooks)
+		go updateLocalCopy(webhooks, sync)
 	}
 }
