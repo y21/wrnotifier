9f9f385d79c781000781825a262d317e28804f10
Timo <artur.fiedler54@gmail.com>
2019-12-18 18:43:53 +0100

    add fetch endpoint

--- api/fetch.go
+++ api/fetch.go
@@ -1,11 +1,22 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+
 	"github.com/y21/wrnotifier-go/structures"
 )
 
 // Fetch is used to get ID and Server of webhook
 func Fetch(w http.ResponseWriter, r *http.Request, webhooks *[]structures.Webhook) {
-
+	data := make([]structures.ExposableWebhook, len(*webhooks))
+	for i, el := range *webhooks {
+		data[i] = structures.ExposableWebhook {
+			EngineClass150: el.EngineClass150,
+			ID: el.ID,
+			Server: el.Server,
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
