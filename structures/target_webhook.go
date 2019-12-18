92b4ae885314a59bc09defef2bc538bc37faf517
Timo <artur.fiedler54@gmail.com>
2019-12-18 21:22:11 +0100

    remove unused structure TargetWebhook

--- structures/target_webhook.go
+++
@@ -1,7 +0,0 @@
-package structures
-
-// ExposableWebhook holds non-sensitive information that can be exposed
-type TargetWebhook struct {
-	ID             string `json:"id"`
-	Server         string `json:"server"`
-}
