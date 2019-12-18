70ede3417a29ec3b790931011c9a164e8cbd7515
Timo <artur.fiedler54@gmail.com>
2019-12-18 22:36:05 +0100

    dont check engine class

--- utils/get_webhook_index.go
+++ utils/get_webhook_index.go
@@ -7,7 +7,7 @@ import (
 // GetExposableWebhookIndex gets the index of a specific exposable webhook in array
 func GetExposableWebhookIndex(webhooks *[]structures.Webhook, webhook structures.ExposableWebhook) int {
 	for i, el := range *webhooks {
-		if el.EngineClass150 == webhook.EngineClass150 && el.ID == webhook.ID && el.Server == webhook.Server {
+		if el.ID == webhook.ID && el.Server == webhook.Server {
 			return i
 		}
 	}
@@ -16,10 +16,9 @@ func GetExposableWebhookIndex(webhooks *[]structures.Webhook, webhook structures
 
 // GetWebhookIndex gets the index of a specific webhook in array
 func GetWebhookIndex(webhooks *[]structures.Webhook, webhook structures.Webhook) int {
-	for i, el := range *webhooks {
-		if el.EngineClass150 == webhook.EngineClass150 && el.ID == webhook.ID && el.Server == webhook.Server && el.Token == webhook.Token {
-			return i
-		}
-	}
-	return -1
+	return GetExposableWebhookIndex(webhooks, structures.ExposableWebhook {
+		EngineClass150: webhook.EngineClass150,
+		ID: webhook.ID,
+		Server: webhook.Server
+	})
 }
