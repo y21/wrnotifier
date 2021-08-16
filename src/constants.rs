7b8edb46542afc37ab1ef7820a1d4794091a9b66
Timo <30553356+y21@users.noreply.github.com>
2021-08-16 03:24:07 +0200

    fix link

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
 pub const DATABASE_PATH: &str = "file:database.sqlite";
-pub const TT_API_BASE: &str = "http://tt.chadsoft.co.uk ";
+pub const TT_API_BASE: &str = "http://tt.chadsoft.co.uk";
 pub const TT_API: &str = "http://tt.chadsoft.co.uk/index.json";
